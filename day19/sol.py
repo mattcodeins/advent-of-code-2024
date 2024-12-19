12f2171b44fd63341223122b7bb6496786fcbaf6
USER <user@USERs-MacBook-Pro.local>
2024-12-19 12:16:22 +0000

    day19

--- day19/sol.py
+++ day19/sol.py
@@ -1,5 +1,24 @@
 from utils import *
 
 inp = readlines('day19/input.txt')
-inp = readlines('day19/test.txt', prnt=True)
+# inp = readlines('day19/test.txt', prnt=True)
 
+patterns = [x.strip(',') for x in inp[0][0]]
+designs = inp[1]
+
+p1 = 0
+p2 = 0
+for d in designs:
+    dp = [0 for _ in range(len(d)+1)]
+    dp[0] = 1
+    for i, x in enumerate(dp):
+        if not(dp[i]):
+            continue
+        for p in patterns:
+            if d[i:i+len(p)] == p:
+                dp[i+len(p)] += dp[i]
+    p1 += bool(dp[-1])
+    p2 += dp[-1]
+
+print(p1)
+print(p2)
